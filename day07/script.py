5a8e1a4240bfa678c494a2803709ed3d75ce67b3
Bob Belderbos <bobbelderbos@gmail.com>
2024-12-07 15:33:09 +0100

    part 2 works but way to slow (20 seconds), need to optimize

--- day07/script.py
+++ day07/script.py
@@ -1,7 +1,7 @@
 from itertools import product
 from operator import add, mul
 from pathlib import Path
-from typing import Generator, NamedTuple
+from typing import Callable, Generator, NamedTuple
 
 
 class Operation(NamedTuple):
@@ -15,12 +15,12 @@ def parse_data(data: str) -> Generator[Operation, None, None]:
         yield Operation(int(res), [int(o) for o in ops.split()])
 
 
-def solve_part1(data: str) -> int:
+def solve_part1(data: str, ops: list[Callable] = [add, mul]) -> int:
     """Solve part 1 of the problem."""
     parsed_data = parse_data(data)
     valid = []
     for operation in parsed_data:
-        combos = product([mul, add], repeat=len(operation.operations) - 1)
+        combos = product(ops, repeat=len(operation.operations) - 1)
         for combo in combos:
             result = operation.operations[0]
 
@@ -34,8 +34,13 @@ def solve_part1(data: str) -> int:
     return sum(valid)
 
 
-# def solve_part2(data: str) -> int:
-#     """Solve part 2 of the problem."""
+def concat(*args):
+    return int("".join(str(a) for a in args))
+
+
+def solve_part2(data: str) -> int:
+    """Solve part 2 of the problem."""
+    return solve_part1(data, ops=[add, mul, concat])
 
 
 def main():
@@ -58,11 +63,11 @@ def main():
     print(f"Part 1: {part1_result}")
     assert part1_result == 5837374519342
 
-    # part2_test = solve_part2(data)
-    # assert part2_test == 0
-    # part2_result = solve_part2(input_file)
-    # print(f"Part 2: {part2_result}")
-    # assert part2_result == 0
+    part2_test = solve_part2(data)
+    assert part2_test == 11387
+    part2_result = solve_part2(input_file)
+    print(f"Part 2: {part2_result}")
+    assert part2_result == 492383931650959
 
 
 if __name__ == "__main__":
