41747d7fb44eee1799ae38417a8d53b695f577ca
Bob Belderbos <bobbelderbos@gmail.com>
2024-12-14 20:21:23 +0100

    part 2

--- day10/script.py
+++ day10/script.py
@@ -28,10 +28,10 @@ def find_paths(grid, x, y, current_value, visited, endpoints):
 
     # 4 possible directions
     paths = (
-        find_paths(grid, x + 1, y, grid[x][y], visited, endpoints)  # South
-        + find_paths(grid, x - 1, y, grid[x][y], visited, endpoints)  # North
-        + find_paths(grid, x, y + 1, grid[x][y], visited, endpoints)  # East
-        + find_paths(grid, x, y - 1, grid[x][y], visited, endpoints)  # West
+        find_paths(grid, x + 1, y, grid[x][y], visited, endpoints)
+        + find_paths(grid, x - 1, y, grid[x][y], visited, endpoints)
+        + find_paths(grid, x, y + 1, grid[x][y], visited, endpoints)
+        + find_paths(grid, x, y - 1, grid[x][y], visited, endpoints)
     )
 
     # backtrack
@@ -51,13 +51,25 @@ def solve_part1(data: str) -> int:
                 find_paths(grid, x, y, -1, set(), endpoints)
                 score = len(endpoints)
                 trailhead_scores.append(score)
-                # print(f"Trailhead at ({x}, {y}) has score: {score}")
 
     return sum(trailhead_scores)
 
 
-# def solve_part2(data: str) -> int:
-#     """Solve part 2 of the problem."""
+def solve_part2(data: str) -> int:
+    """Solve part 2 of the problem."""
+    grid = str_to_grid(data)
+    rows, cols = len(grid), len(grid[0])
+    trailhead_scores = []
+
+    for x in range(rows):
+        for y in range(cols):
+            if grid[x][y] == 0:
+                # for part 2 we can simply stop using unique
+                # endpoints to get all available paths
+                score = find_paths(grid, x, y, -1, set(), set())
+                trailhead_scores.append(score)
+
+    return sum(trailhead_scores)
 
 
 def main():
@@ -79,11 +91,11 @@ def main():
     print(f"Part 1: {part1_result}")
     assert part1_result == 816
 
-    # part2_test = solve_part2(data)
-    # assert part2_test == 0
-    # part2_result = solve_part2(input_file)
-    # print(f"Part 2: {part2_result}")
-    # assert part2_result == 0
+    part2_test = solve_part2(data)
+    assert part2_test == 81
+    part2_result = solve_part2(input_file)
+    print(f"Part 2: {part2_result}")
+    assert part2_result == 1960
 
 
 if __name__ == "__main__":
